5ef4cb033799e26afbc62e081d1f45d9e0b57787
zyc <601905510@qq.com>
2024-05-14 19:24:35 +0800

    02ch_minimal_rust_kernel

---
+++ README.md
@@ -0,0 +1,5 @@
+02ch
+
+命令：
+    1.cargo bootimage 编译生成qemu镜像
+    2.进入bin目录执行 qemu-system-x86_64 -drive format=raw,file=bootimage-blog_os.bin
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-#![no_std]
-#![no_main]
+// src/main.rs
+
+#![no_std] // 不链接Rust标准库
+#![no_main] // 禁用所有Rust层级的入口点
 
 use core::panic::PanicInfo;
 
@@ -9,8 +11,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+// 因为编译器会寻找一个名为`_start`的函数，所以这个函数就是入口点
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
-
