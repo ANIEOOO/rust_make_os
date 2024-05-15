4568f0cdb90b47d7836eebba50a6c4534302240b
zyc <601905510@qq.com>
2024-05-15 19:00:31 +0800

    03ch_vga_text_mode

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,18 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+
 // 因为编译器会寻找一个名为`_start`的函数，所以这个函数就是入口点
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello world!");
+    panic!("Hello world!");
     loop {}
 }
 
 /// 这个函数将在panic时被调用
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,8 @@
+use volatile::Volatile;
 
+use core::fmt::{self, Write};
+use lazy_static::lazy_static;
+use spin::Mutex;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -43,7 +47,112 @@ struct ScreenChar {
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
+
+/* chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT]: 一个二维数组，每行有 BUFFER_WIDTH 个 ScreenChar，
+总共有 BUFFER_HEIGHT 行。这个二维数组用于表示整个屏幕的状态，每个 ScreenChar 存储一个字符及其颜色代码。*/ 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,    // static生命周期表明整个程序运行期间有效
+}
+
+// 创建一个全局静态变量表示屏幕参数
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(), // 写入字符'\n'表示换行
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {   // 换行
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;    // 待写入的行
+                let col = self.column_position; // 待写入的列
+
+                let color_code = self.color_code;   // 待写入的颜色
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code: color_code,
+                });
+                self.column_position += 1;
+            }
+            
+        }
+    }
+
+    fn new_line(&mut self) {/*TODO */
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read(); // 读取字符
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+impl Writer {
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                 // 可以是能打印的ASCII码字节，也可以是换行符
+                 0x20..=0x7e | b'\n' => self.write_byte(byte),
+                 // 不包含上述ascii码则写入0xfe
+                 _ => self.write_byte(0xfe),
+            }
+        }
+    }
+}
+
+// 实现rust格式化宏
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+// This is a check
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+    
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
